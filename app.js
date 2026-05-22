1a4b272b7a6826673ef2fa4694577db0277f7650
Aditi Singh <singhaditi19102004@gmail.com>
2026-05-23 02:35:18 +0530

    Expose explicit home path redirection utility to listings grid layout

--- app.js
+++ app.js
@@ -79,6 +79,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 // 404 Route - Fixed syntax
 app.all(/(.*)/, (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
